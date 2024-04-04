0fd7730cdafabaebe550aec2682145961b7dc946
pedroffd <pedro.alcarin@gmail.com>
2024-04-04 17:34:38 -0300

    chore: background image adjustments for mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,21 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen min-w-screen mx-auto">
       <Header />
-      <div className="flex-grow relative">
-        <div className="absolute inset-0 bottom-12 -top-12 md:-bottom-10 lg:-top-20 lg:-bottom-44">
-          <Image
-            src="/images/jumbotron.jpg"
-            alt="Medroster Landing"
-            placeholder="blur"
-            layout="fill"
-            objectFit="cover"
-            loading="lazy"
-            blurDataURL="data:/jumbotron.jpg"
-          />
-        </div>
-
-        <div className="relative z-10 xl:container">
-          <Hero />
-        </div>
+      <div className="absolute inset-0 bottom-48 md:-bottom-10 lg:-top-20 lg:-bottom-44">
+        <Image
+          src="/images/jumbotron.jpg"
+          alt="Medroster Landing"
+          placeholder="blur"
+          layout="fill"
+          objectFit="cover"
+          loading="lazy"
+          blurDataURL="data:/jumbotron.jpg"
+        />
       </div>
-      <div className="relative -top-12 md:top-5 lg:top-40">
+      <div className="flex-grow relative">
+        <div className="relative z-10 xl:container">{/*    <Hero /> */}</div>
+      </div>
+      <div className="relative -top-11 md:top-5 lg:top-40">
         <CTA />
       </div>
     </main>
