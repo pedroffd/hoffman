dd686c2fd29b70e3400164ca0b379049a81cd484
pedroffd <pedro.alcarin@gmail.com>
2024-04-24 12:56:59 -0300

    chore: New design for faq - terms of service - contact page

--- src/app/components/terms.tsx
+++ src/app/components/terms.tsx
@@ -13,23 +13,10 @@ export interface TermsProps {
 const Terms: React.FC<TermsProps> = ({ terms }) => {
   return (
     <div className="w-full bg-muted relative mx-auto">
-      <div className="relative mx-auto">
-        <Image
-          src="/images/tinted-jumbotron-3.jpg"
-          alt="Terms of Use"
-          placeholder="blur"
-          loading="lazy"
-          height={304}
-          width={1170}
-          layout="responsive" // Adiciona essa propriedade
-          blurDataURL="data:/images/tinted-jumbotron-3.jpg"
-          className="w-full" // Adiciona essa classe
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-2xl sm:text-4xl font-bold text-white">
-            Terms of Service
-          </h1>
-        </div>
+      <div className="h-56 md:h-80 inset-0 flex items-center justify-center bg-secondary">
+        <h1 className="text-2xl sm:text-4xl font-bold text-white">
+          Terms of Service
+        </h1>
       </div>
 
       <div className="text-justify text-text-title justify-center items-center max-w-4xl mx-auto container mt-8">
